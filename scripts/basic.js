6afb73d8b9436447aadce886c8f250d579b529d6
smjaberjawad <smjaberjawad@gmail.com>
2024-02-29 00:33:05 +0600

    34-3 complete

--- scripts/basic.js
+++ scripts/basic.js
@@ -1,28 +1,34 @@
-const loadData = async () => {
-    const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const handleSearch = () => {
+    const searchField = document.getElementById('search_field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    loadData(searchText);
+}
+
+const loadData = async (searchText) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const json = await response.json();
     const phones = json.data;
-    displayPhones(phones)
+    displayPhones(phones);
 }
 
 const displayPhones = (phones) => {
     const phoneContainer = document.getElementById('phone_container');
+    phoneContainer.innerText = '';
     phones.forEach(phone => {
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card w-96 bg-base-100 shadow-xl border-2 border-gray-500 rounded-3xl m-auto`
         phoneCard.innerHTML = `
-            <figure><img src="${phone.image}" alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">${phone.phone_name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
+        <figure><img src="${phone.image}" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+        <h2 class="card-title">${phone.phone_name}</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+        </div>
+        </div>
         `
         phoneContainer.appendChild(phoneCard);
     });
 }
-
-loadData()
