cc3360b32f590ad6389151b825459eeecffaf10e
smjaberjawad <smjaberjawad@gmail.com>
2024-02-29 13:25:33 +0600

    34-7 complete

--- scripts/basic.js
+++ scripts/basic.js
@@ -1,11 +1,3 @@
-const handleSearch = () => {
-    loadingBar(true);
-    const searchField = document.getElementById('search_field');
-    const searchText = searchField.value;
-    searchField.value = '';
-    loadData(searchText);
-}
-
 const loadData = async (searchText) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const json = await response.json();
@@ -13,17 +5,7 @@ const loadData = async (searchText) => {
     displayPhones(phones);
 }
 
-const displayPhones = (phones) => {
-
-    const showAllButton = document.getElementById('show_all_button');
-
-    if (phones.length > 6) {
-        showAllButton.classList.remove('hidden');
-    } else {
-        showAllButton.classList.add('hidden');
-    }
-
-    phones = phones.splice(0, 6);
+const displayPhones = phones => {
 
     const phoneContainer = document.getElementById('phone_container');
     phoneContainer.innerText = '';
@@ -33,13 +15,13 @@ const displayPhones = (phones) => {
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card w-96 bg-base-100 shadow-xl border-2 border-gray-500 rounded-3xl m-auto`
         phoneCard.innerHTML = `
-        <figure><img src="${phone.image}" alt="Shoes" />
+        <figure class="mt-10"><img src="${phone.image}" alt="Shoes" />
         </figure>
-        <div class="card-body">
-        <h2 class="card-title">${phone.phone_name}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+        <div class="card-body flex justify-center items-center">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p class="text-center">If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button onclick="showDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
         </div>
         </div>
         `
@@ -51,6 +33,14 @@ const displayPhones = (phones) => {
 
 }
 
+const handleSearch = () => {
+    loadingBar(true);
+    const searchField = document.getElementById('search_field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    loadData(searchText);
+}
+
 const loadingBar = (isLoading) => {
     const loadingBar = document.getElementById('loading_bar');
     if (isLoading) {
@@ -58,4 +48,10 @@ const loadingBar = (isLoading) => {
     } else {
         loadingBar.classList.add('hidden');
     }
+}
+
+const showDetails = async (id) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await response.json();
+    console.log(data);
 }
